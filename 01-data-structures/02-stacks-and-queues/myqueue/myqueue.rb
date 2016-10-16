8aa328bff31fbaf6e1106c344ce0a74c92270e3d
Drian Hillman <dhillman24@gmail.com>
2016-10-16 16:57:04 +0000

    Assignment 02 Complete! :white_check_mark:

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -3,16 +3,26 @@ class MyQueue
   attr_accessor :tail
 
   def initialize
-    @queue = Array.new
-    @head = @queue[0]
+    @queue = []
+    @head = @queue[-1]
+    @tail = @queue[0]
   end
 
   def enqueue(element)
+    @queue.insert(@queue.length, element)
+    @head = @queue[-1]
+    @tail = @queue[0]
   end
 
   def dequeue
+    temp = @head
+    @queue.delete_at(0)
+    @head = @queue[-1]
+    @tail = @queue[0]
+    return temp
   end
 
   def empty?
+    @queue[0] === nil
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -2,16 +2,23 @@ class MyStack
   attr_accessor :top
 
   def initialize
-    @stack = Array.new
+    @stack = []
     self.top = nil
   end
 
   def push(item)
+    @stack.insert(@stack.length,item)
+    self.top = @stack[-1]
   end
 
   def pop
+    temp = @stack[-1]
+    @stack.delete_at(-1)
+    self.top = @stack[-1]
+    return temp
   end
 
   def empty?
+    @stack[0] === nil
   end
 end
